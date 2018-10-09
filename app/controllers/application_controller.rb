05de5ba834fb42c3725414e3aab34f245ebb564c
Calvin Wong-Loi-Sing <c.wongloising@websend.nl>
2018-10-09 12:33:21 +0200

    Gatekeeper updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception  
   before_action :set_properties
 
-  include Gatekeeper::RequiresAuthentication
-  include Gatekeeper::ProvidesCurrentUser
 
-  before_action { authenticate! }
+
 
   def set_properties
     @activate_repository = false
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_141953) do
+ActiveRecord::Schema.define(version: 2018_07_25_141952) do
 
-  create_table "gatekeeper_department_roles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "gatekeeper_department_roles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.integer "department_id", null: false
     t.integer "role_id"
     t.datetime "created_at"
@@ -20,13 +20,13 @@ ActiveRecord::Schema.define(version: 2018_07_25_141953) do
     t.index ["department_id"], name: "index_gatekeeper_department_roles_on_department_id"
   end
 
-  create_table "gatekeeper_departments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "gatekeeper_departments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "gatekeeper_identities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "gatekeeper_identities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2018_07_25_141953) do
     t.index ["email"], name: "index_gatekeeper_identities_on_email", unique: true
   end
 
-  create_table "gatekeeper_identity_roles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "gatekeeper_identity_roles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.integer "identity_id", null: false
     t.integer "role_id", null: false
     t.integer "department_id"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2018_07_25_141953) do
     t.index ["role_id"], name: "index_gatekeeper_identity_roles_on_role_id"
   end
 
-  create_table "gatekeeper_permissions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "gatekeeper_permissions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "method"
     t.string "controller", null: false
     t.string "action", null: false
@@ -54,10 +54,9 @@ ActiveRecord::Schema.define(version: 2018_07_25_141953) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "engine"
-    t.index ["method", "controller", "action", "engine"], name: "combined_method_controller_action_index", unique: true
   end
 
-  create_table "gatekeeper_permissions_roles", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "gatekeeper_permissions_roles", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.integer "permission_id", null: false
     t.integer "role_id", null: false
     t.index ["permission_id", "role_id"], name: "gatekeeper_combined_permission_role_index", unique: true
@@ -65,19 +64,19 @@ ActiveRecord::Schema.define(version: 2018_07_25_141953) do
     t.index ["role_id"], name: "index_gatekeeper_permissions_roles_on_role_id"
   end
 
-  create_table "gatekeeper_roles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "gatekeeper_roles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name"
-    t.integer "snippets_count", default: 0
+    t.integer "snippets_count", default: 0, null: false
     t.index ["name"], name: "index_labels_on_name", unique: true
   end
 
-  create_table "snippets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "snippets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "language"
@@ -86,23 +85,6 @@ ActiveRecord::Schema.define(version: 2018_07_25_141953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["label_id"], name: "index_snippets_on_label_id"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
